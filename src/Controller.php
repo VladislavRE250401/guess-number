45b4054666a5f9e3409d1e9e1fbc2058d34bb6e1
VladislavRE250401 <99533628+VladislavRE250401@users.noreply.github.com>
2022-09-20 20:40:59 +0300

    Update Controller.php

--- src/Controller.php
+++ src/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace VladislavRE250401\guess_number\Controller;
-use function VladislavRE250401\guess_number\View\showGame;
+namespace vladislav_re250401\guess_number\Controller;
+use function vladislav_re250401\guess_number\View\showGame;
 
 function startGame()
 {
