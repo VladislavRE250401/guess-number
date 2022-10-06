59eb0e85510fe395cab1fb8e71fe2108cf8736d0
VladislavRE250401 <vladislavdohrenaumnyj7268@gmail.com>
2022-10-06 18:12:12 +0300

    change guess-number

--- bin/guess-number.php
+++ bin/guess-number.php
@@ -3,7 +3,7 @@
 $autoLoadGit = __DIR__.'/../vendor/autoload.php';
 $autoLoadPackgaist = __DIR__.'/../../../autoload.php';
 file_exists($autoLoadGit) ? require_once($autoLoadGit) : require_once($autoLoadPackgaist);
-use function vladislav_re250401\guess_number\Controller\startGame;
-startGame();
+use function vladislav_re250401\guess_number\Controller\menu;
+menu();
 
 ?>
